2207374e6bf0c7a06a8cdc60091e1a624ddb876c
Anton Kril <anton.kril@gmail.com>
2023-06-12 23:24:52 -0500

    Added Type Registry and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Config pre-compilation for Magento 2
+
+## Installation
+
+1. Install the components as your dependencies (as 'path'-type repository)
+2. Add component DI configuration to the application `app/etc/di.xml` 
+
---
+++ framework-config/TypeRegistry.php
@@ -0,0 +1,25 @@
+<?php
+namespace Magento\Framework\Config;
+
+use LogicException;
+
+class TypeRegistry 
+{
+    private $types = [];
+
+    public function __construct($types = [])
+    {
+        $this->types = $types;
+    }
+
+    public function getTypeByFileName($fileName) : string
+    {
+        if (!isset($this->types[$fileName])) {
+            throw new LogicException(
+                sprintf('No config type registered for file "%s" in config compiler', $fileName)
+            );
+        }
+        return $this->types[$fileName];
+    }
+}
+
